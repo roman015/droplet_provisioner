c807e4383ea624b3449b7f93507e8b23add45b2d
Abraham V <abraham.varricatt@googlemail.com>
2020-05-01 20:20:08 -0400

    Specify interpreter on script

--- run_ansible.sh
+++ run_ansible.sh
@@ -4,4 +4,6 @@ echo 'move to folder'
 cd ./ansible_config
 
 echo 'run playbook'
-ansible-playbook basic-provision.yml -e working_host=165.227.125.212
+ansible-playbook basic-provision.yml \
+                 -e working_host=165.227.125.212 \
+                 -e 'ansible_python_interpreter=/usr/bin/python3'
