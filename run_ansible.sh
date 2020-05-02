c1554f5254daa05052b14cd14ff466a5c841801c
Abraham V <abraham.varricatt@googlemail.com>
2020-05-02 08:06:25 -0400

    Restructuring ansbile scripts
    Switching over to using roles, stuff placed into folders.

--- run_ansible.sh
+++ run_ansible.sh
@@ -7,3 +7,6 @@ echo 'run playbook'
 ansible-playbook basic-provision.yml \
                  -e working_host=165.227.125.212 \
                  -e 'ansible_python_interpreter=/usr/bin/python3'
+
+# To pass host IP with CLI command, 
+# ref: https://stackoverflow.com/a/49790233/198660
