18d35a15ff19d0582f8a04f1d513d235bc7442d1
roman015 <xaviervarricatt@live.in>
2020-05-04 00:23:18 +0800

    Replaced ip address

--- run_ansible.sh
+++ run_ansible.sh
@@ -5,7 +5,7 @@ cd ./ansible_config
 
 echo 'run playbook'
 ansible-playbook basic-provision.yml \
-                 -e working_host=159.65.244.208 \
+                 -e working_host=128.199.74.127 \
                  -e 'ansible_python_interpreter=/usr/bin/python3'
 
                 #  --key-file ../id_rsa \
